a8266f6d8ce14625e67e1f81aa326e3d6a42baef
Pharmacyst <80787559+Pharmacyst@users.noreply.github.com>
2021-08-23 10:29:21 +0200

    Second commit: Created .js file for the script with into comment

---
+++ mixed_messages.js
@@ -0,0 +1,8 @@
+/*  Script author: Jonathan LAVOIE (PharmaCyst), Copyright 2021
+
+This script is designed to create random mad-lib type messages. The structure of the message is:
+
+"The [adjective] [noun] [past-tense verb] [adverb] in the [noun]."
+
+Each word comes from a separate array.  A random number generator chooses an index for each array, and a function using those indicies as arguments concatenates the selected
+words into the sentence, then returns the sentence. */
