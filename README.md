0f2b86cc694e8db28976f88d48d1d0bd0109f26b
Pharmacyst <80787559+Pharmacyst@users.noreply.github.com>
2021-08-24 05:48:28 +0200

    Added message component arrays, and added program outline to README file

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ This project involves the creation of a Node.js program that creates random mess
 ## Project Objectives
 
 To demonstrate compentence in basic Javascript syntax, Git version control, and command line use.  Furthermore this project will demonstrate an ability to meet (and stick to) specifications.
+
+## High level project outline
+
+**Script author: Jonathan LAVOIE (PharmaCyst), Copyright 2021**
+
+This script is designed to create random mad-lib type messages. The structure of the message is:
+
+"The [*adjective*] [p*rofession*] [*past-tense verb*] [*adverb*] in the [*container*]."
+
+Each word comes from a separate array.  A random number generator chooses an index for each array, and a function using those indicies as arguments concatenates the selected words into the sentence, then returns the sentence.
--- mixed_messages.js
+++ mixed_messages.js
@@ -2,7 +2,20 @@
 
 This script is designed to create random mad-lib type messages. The structure of the message is:
 
-"The [adjective] [noun] [past-tense verb] [adverb] in the [noun]."
+"The [adjective] [profession] [past-tense verb] [adverb] in the [container]."
 
 Each word comes from a separate array.  A random number generator chooses an index for each array, and a function using those indicies as arguments concatenates the selected
 words into the sentence, then returns the sentence. */
+
+// Uncomment next line to see if this script runs in Node
+//console.log('This runs in Node');
+
+//Creating arrays for the message components
+const adjective = ['jovial', 'rustic', 'flatulent', 'easy going', 'cultured', 'sycophantic', 'determined', 'gargantuan', 'short', 'handsome'];
+const profession = ['doctor', 'pornstar', 'professor', 'pharmacist', 'hacker', 'samurai', 'skipper', 'flame eater', 'poop scooper', 'web developper'];
+const past_tense_verb = ['ate', 'jostled', 'meditated', 'urinated', 'wrote a novel', 'coded', 'drove', 'tweeted', 'instagramed', 'burned'];
+const adverb = ['exquisitely', 'jovially', 'intensly', 'passionately', 'humbly', 'aggressively', 'discretely', 'inconsiderately', 'flawlessly', 'lewdly'];
+const container = ['dumpster', 'bathroom', 'kitchen', 'RV', 'theater', 'White House', 'boudoir', 'office', 'fish tank', 'space station'];
+
+//Creating random number generator function
+
